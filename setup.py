c30209df79c17f29977fc4563e5fe34c4fbad7da
Matt Hooks <me@matthooks.com>
2016-06-23 21:30:43 -0400

    Initial commit.  Package structure and setup.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='piixxie',
+    version='0.1.0',
+    packages=['piixxie'],
+    url='https://github.com/Hooksie/piixxie',
+    license='MIT',
+    author='Matt Hooks',
+    author_email='me@matthooks.com',
+    description='A pixel art resizing tool.',
+)
